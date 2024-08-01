da296443e8fecb4ed35518140b7d25d9897429bd
sohampalkrit <sohampalkrit05@gmail.com>
2024-08-01 21:14:38 +0530

    sorting array

--- sorting_array.cpp
+++ sorting_array.cpp
@@ -55,6 +55,49 @@ void insertion_sort(int arr[],int n){
         }
     }
 }
+void merge(vector<int> &arr,int low,int mid,int high){
+    vector<int> temp;
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right<=high){
+         if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+
+         }else{
+            temp.push_back(arr[right]);
+            right++;
+         }
+
+    }    
+    while (left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=right){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+         arr[i]=temp[i-low];
+    }
+
+    
+     
+}
+void ms(vector<int> &arr,int low, int high){
+    if(low==high) return;
+    int mid = (high+low)/2;
+    ms(arr,low,mid);
+    ms(arr,mid+1,high);
+    merge(arr,low,mid,high);
+}
+void merge_sort(vector<int> &arr,int n){
+    ms(arr,0,n-1);
+     
+
+}
 int main(){
     int n;
     cin>>n;
@@ -65,6 +108,7 @@ int main(){
     //selection_sort(arr,n);
     //Bubble_sort(arr,n);
     //insertion_sort(arr,n);
+    
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<endl;
